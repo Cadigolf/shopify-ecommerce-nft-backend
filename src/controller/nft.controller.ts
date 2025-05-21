043a79a6faf9b3c69f887661ed8cd0846fdb3fb5
Renan Braga <renan.work32@gmail.com>
2025-05-21 16:09:20 -0300

    feat: add feature to avoid cors error

--- src/controller/nft.controller.ts
+++ src/controller/nft.controller.ts
@@ -30,7 +30,7 @@ export const buyProductController = async (req: Request, res: Response) => {
                     } else {
                         res.status(400).json({ message: 'Failed to transfer NFT' });
                     }
-                }, 10000);
+                }, 15000);
             } else {
                 res.status(400).json({ message: 'Failed to mint NFT' });
             }
