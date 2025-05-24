f95ce9c1a9c6764b60fa195ca4980a7a0e70bfb8
Renan Braga <renan.work32@gmail.com>
2025-05-24 18:21:20 -0300

    feat: fix the orders/paid to get product info

--- src/index.ts
+++ src/index.ts
@@ -9,10 +9,10 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 1001;
 const corsOptions = {
-    origin: '*', // Allow all origins
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'FETCH'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
+  origin: '*', // Allow all origins
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'FETCH'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
 };
 
 app.use(cors(corsOptions));
@@ -23,33 +23,14 @@ app.use(express.raw());
 
 app.use('/api', router);
 
-// app.post('/webhooks/orders/create', express.json(), (req, res) => {
-//     const orderData = req.body;
-//     console.log('New Order:', orderData);
-//     res.status(200).send('Webhook received');
-// });
-
-app.post('/webhooks/orders/create', express.raw({ type: 'application/json' }), (req, res) => {
-    console.log("First result", req.body);
-    const rawBody = req.body.toString('utf8');
-    console.log("Second result", rawBody);
-    // const orderData = JSON.parse(rawBody);
-    // console.log("Third result", orderData);
-    const products = rawBody.line_items.map((item: any) => ({
-        name: item.title,
-        price: item.price,
-        quantity: item.quantity,
-        image: item.image ? item.image.src : null,  // May be null
-        product_id: item.product_id,
-        variant_id: item.variant_id
-    }));
-
-    console.log("✔️✔️✔️", products);
-    res.status(200).send('Webhook received');
+app.post('/webhooks/orders/paid', express.json(), (req, res) => {
+  const orderData = req.body;
+  console.log('New Order:', orderData);
+  res.status(200).send('Webhook received');
 });
 
 SupabaseConnection();
 
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+  console.log(`Server is running on port: ${port}`);
 });
