817a9d7c6327aef3ead917be52adb39218245c8d
Renan Braga <renan.work32@gmail.com>
2025-05-25 07:12:05 -0300

    fix: add the console.log step by step

--- src/index.ts
+++ src/index.ts
@@ -29,7 +29,7 @@ SupabaseConnection();
 
 app.post('/webhooks/orders/paid', express.json(), async (req: Request, res: Response): Promise<void> => {
   try {
-    console.log("\n\n1: ", req.body, "\n\n\n\n");
+    console.log("👌👌👌👌👌👌👌👌👌👌👌👌👌👌👌👌👌👌👌👌👌👌👌👌👌");
     const result = await ProductService.getUserProductHistoryProductId(req.body.contact_email, req.body.id);
     if(result){
       await buyProductController(req, res);
