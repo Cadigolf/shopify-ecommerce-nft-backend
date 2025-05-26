1ee1e4c14e0c00d80ed0ffebf817a10abd6c2c0c
Renan Braga <renan.work32@gmail.com>
2025-05-26 07:25:14 -0300

    feat: add the public RESTful API

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,7 @@
 import express, { RequestHandler } from "express";
 import { UserController } from "./controller/user.controller";
 import { getAllProductsController } from "./controller/nft.controller";
+import { PublicController } from "./controller/public.controller";
 
 const router = express.Router();
 
@@ -8,5 +9,10 @@ router.post('/addUser', UserController.addUser as RequestHandler);
 router.get('/getAllUsers', UserController.getAllUsers as RequestHandler);
 router.post('/getUserNFTs', UserController.getUserNFTs as RequestHandler);
 router.get('/getAllProducts', getAllProductsController as RequestHandler);    
+
+// Public Routes
+router.post('/public/mintNFT', PublicController.mintNFT as RequestHandler);
+router.get('/public/wallet/create', PublicController.createWallet as RequestHandler);
+router.post('/public/transferNFT', PublicController.transferNFT as RequestHandler);
 
 export default router;
--- src/utils/publicFeatures.ts
+++ src/utils/publicFeatures.ts
@@ -9,20 +9,14 @@ dotenv.config();
 
 const solanaRpcUrl = process.env.SOLANA_RPC_URL;
 const umi = createUmi(solanaRpcUrl || "");
-const privateKey = process.env.SOLANA_WALLET_PRIVATEKEY;
-
-const pinataApiKey = process.env.PINATA_API_KEY;
-const pinataSecretApiKey = process.env.PINATA_SECRET_API_KEY;
-
-const umiKeypair = umi.eddsa.createKeypairFromSecretKey(bs58.decode(privateKey || ""));
-umi.use(keypairIdentity(umiKeypair))
-    .use(mplTokenMetadata())
-    .use(mockStorage());
 
 export const createWallet = async () => {
     const wallet = generateSigner(umi);
     return { publicKey: wallet.publicKey.toString(), privateKey: bs58.encode(wallet.secretKey) };
 }
+
+const pinataApiKey = process.env.PINATA_API_KEY;
+const pinataSecretApiKey = process.env.PINATA_SECRET_API_KEY;
 
 async function uploadMetadataToPinata(metadata: any): Promise<string> {
     try {
@@ -44,8 +38,13 @@ async function uploadMetadataToPinata(metadata: any): Promise<string> {
     }
 }
 
-export const mintNFT = async (metadata: any): Promise<string> => {
+export const mintNFT = async (privateKey: string, metadata: any): Promise<string> => {
     try {
+        const umiKeypair = umi.eddsa.createKeypairFromSecretKey(bs58.decode(privateKey || ""));
+        umi.use(keypairIdentity(umiKeypair))
+            .use(mplTokenMetadata())
+            .use(mockStorage());
+
         const mint = generateSigner(umi);
         const minimalMetadata = {
             name: metadata.title,
@@ -85,7 +84,8 @@ export const mintNFT = async (metadata: any): Promise<string> => {
                 preflightCommitment: "confirmed"
             }
         });
-        console.log("2️⃣ NFT minted successfully!");
+        console.log("NFT minted successfully!");
+        console.log(`https://explorer.solana.com/address/${mint.publicKey.toString()}?cluster=devnet`);
         return mint.publicKey.toString()
     } catch (error: any) {
         console.error("ERROR------> NFT minting failed:", error);
@@ -121,11 +121,16 @@ export const burnNFT = async (privateKey: string, mintAddress: string) => {
     }
 };
 
-export const transferNFT = async (mintAddress: string, toAddress: string) => {
+export const transferNFT = async (privateKey: string, mintAddress: string, toAddress: string) => {
     try {
+        const umiKeypair = umi.eddsa.createKeypairFromSecretKey(bs58.decode(privateKey || ""));
+        umi.use(keypairIdentity(umiKeypair))
+            .use(mplTokenMetadata())
+            .use(mockStorage());
         const mint = publicKey(mintAddress);
         const to = publicKey(toAddress);
-        await transferV1(umi, {
+
+        const result = await transferV1(umi, {
             mint,
             authority: umi.identity,
             tokenOwner: umi.identity.publicKey,
@@ -134,8 +139,9 @@ export const transferNFT = async (mintAddress: string, toAddress: string) => {
         }).sendAndConfirm(umi, {
             send: { commitment: "finalized" }
         });
-        console.log("3️⃣ NFT transferred successfully!");
-        return true;
+        console.log("NFT transferred successfully!");
+        const txHash = bs58.encode(result.signature);
+        return { success: true, txHash };
     } catch (error: any) {
         const errorMessage = error.message || "Unknown error occurred while transferring NFT";
         console.error("ERROR------> NFT transferring failed:", errorMessage);
